60f96868f33fc0cf62a4c6390f8a6b76e2c1de6d
Lawrence Kay <lkay9495@hotmail.com>
2018-12-05 02:29:51 +0900

    OneDrive upload Middleware Format Implemented * [Action Required] Need to think what should be returned

--- samples/index.js
+++ samples/index.js
@@ -1,21 +1,12 @@
 var http = require("http");
 var express = require("express");
 var app = express();
-var uploader = require("express-uploader");
+var uploader = require("../lib");
 
 app.server = http.createServer(app);
 
-// If target service is S3
-app.use(uploader({
-  provider: "aws-s3"
-}));
+app.post("/", uploader({provider: "onedrive", path: ""}), (req, res) => {
+  res.sendStatus(200);
+});
 
-// If target service is Google Drive
-app.use(uploader({
-  provider: "google-drive",
-  authKey: "abcd123"
-  // Best format for now, but method could change
-  // depending on how auth should be done
-}));
-
-
+app.server.listen(8080);
