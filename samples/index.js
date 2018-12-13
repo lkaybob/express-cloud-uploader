e3aaacab059a5d1c5d7cfd68a2e1918bafd737e2
Dev_ES <anderson93@ajou.ac.kr>
2018-12-13 22:07:56 +0900

    uploading s3 completed

--- samples/index.js
+++ samples/index.js
@@ -5,23 +5,10 @@ var uploader = require("../lib");
 var opn = require('opn');
 
 
-opn("http://localhost:8080");
 app.server = http.createServer(app);
-// opn('http://localhost:8080');
 
-// app.post("/", uploader({provider: "onedrive", path: ""}), (req, res) => {
-//   res.sendStatus(200);
-// });
-
-// app.get("/", uploader({provider: "aws-s3"}), (req, res)=>{
-//     res.sendStatus(200);
-// });
-
-
-app.get('/', uploader({provider: "aws-s3"}), (req, res)=> {
+app.post('/', uploader({provider: "aws-s3", bucket: "osswtest"}), (req, res)=>{
     res.sendStatus(200);
 });
-
-
 
 app.server.listen(8080);
